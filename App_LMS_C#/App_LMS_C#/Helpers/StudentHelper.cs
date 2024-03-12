292824f2108c834faaf6afa6d5bd0a61d131c0db
Madeline Bramson <madeline.bramson@gmail.com>
2024-03-11 22:25:09 -0400

    Add functionality for searching for students

--- App_LMS_C#/App_LMS_C#/Helpers/StudentHelper.cs
+++ App_LMS_C#/App_LMS_C#/Helpers/StudentHelper.cs
@@ -49,6 +49,14 @@ namespace App_LMS_C_.Helpers
         {
             studentService.Students.ForEach(Console.WriteLine);
         }
+
+        public void SearchStudents()
+        {
+            Console.WriteLine("Enter a query:");
+            var query = Console.ReadLine() ?? string.Empty;
+
+            studentService.Search(query).ToList().ForEach(Console.WriteLine);
+        }
     }
 
 }
