2ad517f8988c9e623e5881e7a939df84845ea8f5
Madeline Bramson <madeline.bramson@gmail.com>
2024-03-11 23:37:39 -0400

    Adding update functionality for courses

--- App_LMS_C#/App_LMS_C#/Program.cs
+++ App_LMS_C#/App_LMS_C#/Program.cs
@@ -19,8 +19,9 @@ namespace App_LMS
                 Console.WriteLine("3. List all enrolled students");
                 Console.WriteLine("4. Search for a student");
                 Console.WriteLine("5. Add a new course");
-                Console.WriteLine("6. List all courses");
-                Console.WriteLine("7. Exit");
+                Console.WriteLine("6. Update a course");
+                Console.WriteLine("7. List all courses");
+                Console.WriteLine("8. Exit");
                 var input = Console.ReadLine();
 
                 if (int.TryParse(input, out int result))
@@ -47,9 +48,13 @@ namespace App_LMS
                     }
                     else if (result == 6)
                     {
-                        courseHelper.ListCourses();
+                        courseHelper.UpdateCourseRecord();
                     }
                     else if (result == 7)
+                    {
+                        courseHelper.ListCourses();
+                    }
+                    else if (result == 8)
                     {
                         cont = false;
                     }
